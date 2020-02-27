5cb447a2490feeea42c4c99083de56b5fcbb2286
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-27 12:03:10 -0800

    authorization and login work

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import React, { Component } from 'react'
 import './App.css';
 import { 
   Route, 
-  Link,
-  // Switch,
+  // Link,
+  Switch,
+  Redirect,
   BrowserRouter as Router, 
 } from 'react-router-dom';
 import TodoApp from './TodoApp.js';
+import TodoAppLogin from './TodoAppLogin.js';
+
+const isLoggedIn = () => JSON.parse(localStorage.getItem('user'));
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        {/* <Switch> */}
+        <Switch>
           <div>
             <h1>Here's what you have to do:</h1>
-            <Link to="/">Home</Link>
-            <Route exact path="/" component={TodoApp} />
+            <Route path='/' render={() => 
+              isLoggedIn()
+                ? <TodoApp />
+                : <Redirect to='login' />
+            } />
+            <Route path='/login' component={TodoAppLogin} />
+
+            {/* <Route exact path="/" component={TodoAppLogin} /> */}
+            {/* <Route exact path="/todos" component={TodoApp} /> */}
           </div>
-        {/* </Switch> */}
+        </Switch>
       </Router>
     )
   }
